7370fefb176d73d5ee965ded39b626b3bd3141c5
Lubosy <amatendelubosi@gmail.com>
2024-06-07 23:31:08 +0200

    Added jenkins endpoint

---
+++ src/main/java/com/example/JenkinsDemo/JenkinsController.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsDemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+
+    @GetMapping("text")
+    public String getText(){
+        return "Jenkins CI CD Complete";
+    }
+}
---
+++ src/test/java/com/example/JenkinsDemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JenkinsDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
